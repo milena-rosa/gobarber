9d13589b77f6b2afc94678cb26d9a32888c74aa2
Milena Boselli Rosa <milena.rosa@gmail.com>
2020-07-05 17:49:55 -0300

    adicionado modulo de notifications e funcionalidade de notificaçao do prestador quando um agendamento eh marcado com ele/a.

--- server/source/modules/appointments/services/CreateAppointmentService.ts
+++ server/source/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
 
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -18,6 +19,9 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({
@@ -57,6 +61,13 @@ class CreateAppointmentService {
       user_id,
       provider_id,
       date: appointmentDate,
+    });
+
+    const formattedDate = format(appointmentDate, "dd.MM.yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${formattedDate}`,
     });
 
     return appointment;
